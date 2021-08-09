82119d0390c75d9fc84b5103015d372d2b544fa6
Jonathan Rein <jrein1296@gmail.com>
2021-08-09 17:19:52 -0400

    Adding home component.

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
