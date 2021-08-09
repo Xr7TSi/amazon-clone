e95ac04a68e6605bdbaa344f680f34cffb0c6e5b
Jonathan Rein <jrein1296@gmail.com>
2021-08-09 19:51:31 -0400

    Adding Product component and styling.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -9,6 +10,19 @@ function Home() {
           src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg"
           alt="Background Image"
         />
+
+        <div className="home_row">
+           <Product /> 
+        </div>
+
+        <div className="home_row">
+            
+        </div>
+
+        <div className="home_row">
+            
+        </div>
+
       </div>
     </div>
   );
---
+++ src/Product.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Product.css"
+
+function Product() {
+    return (
+        <div className="product">
+            <div className="product_info">
+                <p>Sawyer Mini Filter</p>
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product_rating">
+                    <p>⭐</p>
+                    <p>⭐</p>
+                    <p>⭐</p>
+                </div>
+            </div>
+            <img src="https://m.media-amazon.com/images/I/81Dhvd3jvCL._AC_SY400_.jpg" alt=""/>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
