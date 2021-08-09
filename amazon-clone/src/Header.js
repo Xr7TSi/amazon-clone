4397de52083787efa73c06c708aacbb3ace7089e
Jonathan Rein <jrein1296@gmail.com>
2021-08-09 16:28:11 -0400

    Adding gitignore file.  Completing header styling.

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -10,24 +11,27 @@ function Header() {
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
       <div className="header_search">
-        <input className="header_search_input" type="text" />
+        <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
       </div>
 
       <div className="header_nav">
-          <div className="header_option">
-              <span className='header_optionLineOne'>Hello Guest</span>
-              <span className='header_optionLineTwo'>Sign In</span>
-          </div>
-          <div className="header_option">
-              <span className='header_optionLineOne'>Returns</span>
-              <span className='header_optionLineTwo'>& Orders</span>
-          </div>
-          <div className="header_option">
-              <span className='header_optionLineOne'>Your</span>
-              <span className='header_optionLineTwo'>Prime</span>
-          </div>
-         
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_optionLineTwo header_basketCount">0</span>
+        </div>
       </div>
     </div>
   );
