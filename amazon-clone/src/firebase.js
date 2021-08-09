a6e753c0b1f13e385283f8fd6fb0496fa6c5db67
Jonathan Rein <jrein1296@gmail.com>
2021-08-08 21:56:04 -0400

    Adding src/firbase.js for firebase config.

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDY6fgcBl5w-54JXHMuDIZRJe1-Zuh152Q",
+    authDomain: "clone-14498.firebaseapp.com",
+    projectId: "clone-14498",
+    storageBucket: "clone-14498.appspot.com",
+    messagingSenderId: "991340167259",
+    appId: "1:991340167259:web:bb0a6e39d71015b6fe3f7e",
+    measurementId: "G-FV2FFT9NTL"
+  };
