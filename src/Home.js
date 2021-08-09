53a80fca9b6e94d540ff35b45262a3a6aa9344f6
Jonathan Rein <jrein1296@gmail.com>
2021-08-09 17:31:31 -0400

    Adding home background image and home_image styling.

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,9 @@ function Home() {
   return (
     <div className="home">
       <div className="home_container">
-        <img
+        <img className="home_image"
           src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg"
-          alt=""
+          alt="Background Image"
         />
       </div>
     </div>
