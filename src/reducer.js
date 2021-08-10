c98aa9699cab72bb41ce93871dee3cacaf335480
Jonathan Rein <jrein1296@gmail.com>
2021-08-10 16:46:41 -0400

    Add to basket now works.

--- src/reducer.js
+++ src/reducer.js
@@ -20,8 +20,8 @@ const reducer = (state, action) => {
         //         ...state,
         //         user: action.user,
         //     }
-        // default:
-        //     return state
+        default:
+            return state
     }
 }
 
