cc06529ad3e88fdbb3aa0dbbea88c0284f3798fe
Jonathan Rein <jrein1296@gmail.com>
2021-08-10 13:13:08 -0400

    Adding link to shopping cart on heaedr cart image.

--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,12 @@ function Header() {
   return (
     <div className="header">
       <Link to="/">
-      <img
-        className="header_logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+        <img
+          className="header_logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
       </Link>
-     
+
       <div className="header_search">
         <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
@@ -32,10 +32,12 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-        <div className="header_optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header_optionLineTwo header_basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
