7947efee67a308a0f1681fe7f5cfe726f98adce0
Jonathan Rein <jrein1296@gmail.com>
2021-08-10 16:23:27 -0400

    Refactor of src/index.js for serviceWorker.  Adding src/serviceWorker.js.

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,11 +1,15 @@
 import React, { createContext, useContext, useReducer } from "react";
 
+// creates data layer
 export const StateContext = createContext();
 
+
+// provides data layer to all components in the app
 export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
 );
 
+// pulls info from the data layer
 export const useStateValue = () => useContext(StateContext);
