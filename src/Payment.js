6d995f27bf665565b2e643078375eb2f96b277fe
Jonathan Rein <jrein1296@gmail.com>
2021-08-12 15:33:51 -0400

    Adding styling to Payment component.

--- src/Payment.js
+++ src/Payment.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+
 
 function Payment() {
 
@@ -9,20 +12,49 @@ function Payment() {
   return (
     <div className="payment">
       <div className="payment_container">
+          <h1>
+              Checkout (<Link to="/Checkout">{cart?.length} items</ 
+              Link>)
+              
+          </h1>
         <div className="payment_section">
             <div className="payment_title">
                 <h3>Delivery Address</h3>
             </div>
             <div className="payment_address">
-               <p>{user?.email}</p> 
+               <p>{user?.email}</p>
+               <p>123 Happy Street</p> 
+               <p>Carlsbad, CA</p> 
             </div>
         </div>
 
-        <div className="payment_section"></div>
-        <div className="payment_section"></div>
-        <div className="payment_section"></div>
-        <div className="payment_section"></div>
-        <div className="payment_section"></div>
+        <div className="payment_section">
+        <div className="payment_title">
+                <h3>Review Items and Delivery</h3>
+            </div>
+            <div className="payment_items">
+                {cart.map(item => (
+                    <CheckoutProduct
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating}
+                    />
+                ))}
+            </div>
+        </div>
+
+
+        <div className="payment_section">
+            <div className="payment_title">
+                <h3>Payment Method</h3>
+            </div>
+            <div className="payment_details">
+
+            </div>
+        </div>
+     
       </div>
     </div>
   );
