b4dcb1c5689c2a45e37554057314c980dfbe3364
Jonathan Rein <jrein1296@gmail.com>
2021-08-12 22:10:31 -0400

    Updates to Payment.js.  Adding axios.js.

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 import { Link } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from 'react-currency-format';
+import { getCartTotal } from "./reducer";
 
 
 function Payment() {
@@ -12,6 +14,37 @@ function Payment() {
 
     const stripe = useStripe();
     const elements = useElements();
+    
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+    
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const [clientSecret, setClientSecret] = useState(true);
+
+    useEffect(() => {
+        // generate stripe secret for transaction
+        const getClientSecret = async () => {
+            const response = await axios
+        }
+    
+    }, [cart]);
+
+    const handleSubmit =  async (event) => {
+        event.preventDefault();
+        // set processing to true disables the buy now button so customer cant send multiple buy now requests
+        setProcessing(true) 
+
+        // const payload = await stripe
+        
+    }
+
+    const handleChange = event => {
+        setDisabled(event.empty);
+        // if there is an error, show error.  otherwise, show nothing
+        setError(event.error ? event.error.message : "")
+    }
 
   return (
     <div className="payment">
@@ -55,7 +88,31 @@ function Payment() {
                 <h3>Payment Method</h3>
             </div>
             <div className="payment_details">
+                <form onSubmit={handleSubmit}>
+                    <CardElement onChange={handleChange}/>
 
+                    <div className="payment_priceContainer">
+                        <CurrencyFormat
+                        renderText={(value) => (
+                        <>
+                        <h3>Order Total: {value}</h3>
+                        </>
+                        )}
+                        decimalScale={2}
+                        value={getCartTotal(cart)}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={"$"}
+                        />
+                        <button disabled={processing || disabled || succeeded}>
+                        {/* if processing, show Processing, if not, show Buy Now */}
+                        <span>{processing ? <p>Processing</p> :
+                        "Buy Now"}</span>
+                        </button>
+                    </div>
+                    {/* if there is an error, show error */}
+                    {error && <div>{error}</div>}
+                </form>
             </div>
         </div>
      
