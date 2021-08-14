31865dbb4721ab77f31292773b420c2cc58a68f5
Jonathan Rein <jrein1296@gmail.com>
2021-08-13 20:05:47 -0400

    Adding Order.js, Order.css.  Adding styling to Order.css and Orders.css.

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Orders.css"
+import { useStateValue } from "./StateProvider";
+import { db } from "./firebase";
+import Order from "./Order";
 
 function Orders() {
-    return (
-        <div div className="orders">
+    const [{ cart, user }, dispatch] = useStateValue();
+    const [orders, setOrders] = React.useState([])
+
+    useEffect(() => {
+        if(user) {
+            db
+            // access users collection from firebase db
+            .collection('users')
+            // get user's id
+            .doc(user?.uid)
+            // get user's orders
+            .collection('orders')
+            // 
+            .orderBy('created', 'desc')
+            .onSnapshot(snapshot => (
+                // maps through all items ordered  
+                setOrders(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                })))
+            ))
+        } else {
+            setOrders([])
+        }
+    
+      }, [user])
+
+      return (
+        <div className='orders'>
             <h1>Your Orders</h1>
+
+            <div className='orders__order'>
+                {orders?.map(order => (
+                    <Order order={order} />
+                ))}
+            </div>
         </div>
     )
 }
