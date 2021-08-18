230b04d6ce49163e463d7d8d8ed7c938e4bc8e66
Jonathan Rein <jrein1296@gmail.com>
2021-08-17 21:33:01 -0400

    Updates to readme.md.

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDY6fgcBl5w-54JXHMuDIZRJe1-Zuh152Q",
     authDomain: "clone-14498.firebaseapp.com",
     projectId: "clone-14498",
-    storageBucket: "clone-14498.appspot.com",
+    storageBucket: ".appspot.com",
     messagingSenderId: "991340167259",
     appId: "1:991340167259:web:bb0a6e39d71015b6fe3f7e",
     measurementId: "G-FV2FFT9NTL"
